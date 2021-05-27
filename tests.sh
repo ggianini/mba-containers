2e5588649e4e452cac9553cf3b3ca9166e969f75
Gustavo Gianini <50370164+ggianini@users.noreply.github.com>
2021-05-27 04:03:24 -0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -9,13 +9,18 @@ sudo docker build -t mba-mysql-image:1.0 mysql/
 sudo docker run --name mba-nginx-container -d -p 8081:80 mba-nginx-image:1.0
 
 #Assign nginx container URI to variable
+
 NGINX_CONTAINER_IP=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mba-nginx-container)
+echo "\nThe Nginx container IP is $NGINX_CONTAINER_IP"
 
 #Curl from host to container IP on container-nginx port 80 (Nginx default)
+echo "\nCurl from host to container IP"
 curl -s $NGINX_CONTAINER_IP | grep 'Welcome'
 
 #Curl from host to container IP on host port 8081 (forwarded from container 80 -> 8081)
+echo "\nCurl from host to host IP (port forwarded)"
 curl -s http://localhost:8081 | grep 'Welcome'
 
 #Curl from remote container-mysql to container-nginx IP on container nginx port 80 (Nginx default)
+echo "\nCurl from mysql container to container IP"
 sudo docker run --rm --name mba-mysql-container -it mba-mysql-image:1.0 "curl -s '$NGINX_CONTAINER_IP'" | grep 'Welcome'
